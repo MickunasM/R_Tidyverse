35bb64bf8b1885bae607fbd3cbcec06f3e09031c
MickunasM <m.mickunas@outlook.com>
2023-06-10 15:36:31 +0100

    10/06/23 Update 2

--- Code/Connect R with GitHub.R
+++ Code/Connect R with GitHub.R
@@ -1,7 +1,7 @@
 library(usethis)
 
-use_git_config(user.name = "MickunasM", #your github user name
-               user.email = "m.mickunas@outlook.com") # the email you used when you signed upo with github
+use_git_config(user.name = "--", #your github user name
+               user.email = "--") # the email you used when you signed upo with github
 
 # In the RStudio console type the following to generate the PAT 
 usethis::create_github_token()
